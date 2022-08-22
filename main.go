4e746ae133cb36a5ace37b0356650d36f1691192
yangjun <jun.yang@shopee.com>
2022-08-22 12:10:41 +0800

    modified main func

--- main.go
+++ main.go
@@ -67,8 +67,12 @@ func setupRouter() *gin.Engine {
 	return r
 }
 
+// func main() {
+// 	r := setupRouter()
+// 	// Listen and Server in 0.0.0.0:8080
+// 	r.Run(":8080")
+// }
+
 func main() {
-	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	// Hello test git merge
 }
